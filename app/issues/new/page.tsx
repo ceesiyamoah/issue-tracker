e9fb19dca1a95bd0be9006456da2cdd59cd38001
Cyril <ccyamoah@gmail.com>
2024-05-16 22:53:41 +0000

    error message component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -9,6 +9,7 @@ import { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/validationSchema';
 import { z } from 'zod';
+import ErrorMessage from '@/app/components/ErrorMessage';
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -42,17 +43,9 @@ const NewIssuePage = () => {
 			)}
 			<form className='space-y-3' onSubmit={handleSubmit(createIssue)}>
 				<TextField.Root placeholder='Title' {...register('title')} />
-				{errors.title && (
-					<Text color='red' as='p'>
-						{errors.title.message}
-					</Text>
-				)}
+				<ErrorMessage>{errors.title?.message}</ErrorMessage>
 				<Controller name='description' control={control} render={({ field }) => <SimpleMDE {...field} />} />
-				{errors.description && (
-					<Text color='red' as='p'>
-						{errors.description.message}
-					</Text>
-				)}
+				<ErrorMessage>{errors.description?.message}</ErrorMessage>
 
 				<Button>Submit New Issue</Button>
 			</form>
