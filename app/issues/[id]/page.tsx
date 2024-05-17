e2cfabe5983e16a14cb90161368828b33c9991f0
Cyril <ccyamoah@gmail.com>
2024-05-17 18:07:54 +0000

    disable ssr for simplemde editor

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,6 +1,7 @@
 import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 import prisma from '@/prisma/client';
 import { Card, Flex, Heading, Text } from '@radix-ui/themes';
+import delay from 'delay';
 import { notFound } from 'next/navigation';
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
@@ -16,6 +17,7 @@ const IssueDetailPage = async ({ params: { id } }: Props) => {
 		},
 	});
 	if (!issue) notFound();
+	await delay(3000);
 
 	console.log(issue);
 
