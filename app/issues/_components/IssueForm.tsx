38723ce41a7460dd60acc203dbe3f5da23e448d4
Cyril <ccyamoah@gmail.com>
2024-05-18 19:30:16 +0000

    improve loading experience

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,20 +1,18 @@
 'use client';
-import { Button, Callout, Text, TextField } from '@radix-ui/themes';
-import 'easymde/dist/easymde.min.css';
-import { useForm, Controller, SubmitHandler } from 'react-hook-form';
-import axios from 'axios';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { issuesSchema } from '@/app/validationSchema';
-import { z } from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
 import Spinner from '@/app/components/Spinner';
-import dynamic from 'next/dynamic';
+import { issuesSchema } from '@/app/validationSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Issue } from '@prisma/client';
-const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
-	ssr: false,
-});
+import { Button, Callout, TextField } from '@radix-ui/themes';
+import axios from 'axios';
+import 'easymde/dist/easymde.min.css';
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import SimpleMDE from 'react-simplemde-editor';
+import { z } from 'zod';
 
 type IssueFormData = z.infer<typeof issuesSchema>;
 
