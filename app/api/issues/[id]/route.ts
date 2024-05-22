9c6e2330f7d709696086103da7a4561374dd4049
Cyril <ccyamoah@gmail.com>
2024-05-22 23:15:59 +0000

    enhance api to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,4 +1,4 @@
-import { issuesSchema } from '@/app/validationSchema';
+import { issuesSchema, patchIssueSchema } from '@/app/validationSchema';
 import prisma from '@/prisma/client';
 import { getServerSession } from 'next-auth';
 import { NextRequest, NextResponse } from 'next/server';
@@ -16,10 +16,21 @@ export async function PATCH(req: NextRequest, { params: { id } }: WithParams) {
 		return NextResponse.json({}, { status: 400 });
 	}
 	const body = await req.json();
-	const { success, data, error } = issuesSchema.safeParse(body);
+	const { success, data, error } = patchIssueSchema.safeParse(body);
 	if (!success) {
 		return NextResponse.json({ error: error.format() }, { status: 400 });
 	}
+
+	const { assignedToUserId } = data;
+	if (assignedToUserId) {
+		const user = await prisma.user.findUnique({
+			where: {
+				id: assignedToUserId,
+			},
+		});
+		if (!user) return NextResponse.json({ error: 'Invalid user' }, { status: 400 });
+	}
+
 	const issue = await prisma.issue.findUnique({
 		where: {
 			id: +id,
--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -2,5 +2,10 @@ import { z } from 'zod';
 
 export const issuesSchema = z.object({
 	title: z.string().max(255).min(1, 'Title is required'),
-	description: z.string().min(1, 'Description is required'),
+	description: z.string().min(1, 'Description is required').max(65535),
+});
+export const patchIssueSchema = z.object({
+	title: z.string().max(255).min(1, 'Title is required').optional(),
+	description: z.string().min(1, 'Description is required').max(65535).optional(),
+	assignedToUserId: z.string().min(1, 'AssignedToUserId is required').max(255).optional().nullable(),
 });
