a02ed1e6c088a790dce90375fa763b381c6de19a
Cyril <ccyamoah@gmail.com>
2024-05-22 22:49:23 +0000

    add assigned issues to prisma schema

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -3,6 +3,5 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
 	const users = await prisma.user.findMany({ orderBy: { name: 'desc' } });
-
 	return NextResponse.json(users);
 }
